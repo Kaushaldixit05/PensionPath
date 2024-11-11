039d867202fadac8ffd122cd7c95666b9e0487c3
Kaushal Sanjayrao Dixit <143341100+Kaushaldixit05@users.noreply.github.com>
2024-11-11 21:39:37 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ from investment_calculator import calculate_investment_strategy
 app = Flask(__name__)
 
 # Allow only your Netlify domain to access the backend
-CORS(app, origins=["https://pensionpath.netlify.app"])
+CORS(app)
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
